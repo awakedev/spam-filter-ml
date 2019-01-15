93be36da56e480878a6b5320373ef63025d5befd
Nathan Corry <32592701+Wander1ust@users.noreply.github.com>
2019-01-15 17:35:32 +0000

    pre-processing

--- filter.py
+++ filter.py
@@ -2,30 +2,60 @@ import os
 
 from collections import Counter
 
-direc = "email/"
 
-files = os.listdir(direc)
+def make_dict():
+    direc = "email/"
+
+    files = os.listdir(direc)
+
+    # Append directory email to file names
+    emails = [direc + email for email in files]
+
+    words = []
+    ctr = len(emails)
+    for email in emails:
+        f = open(email,  errors='ignore')
+        # Read email
+        blob = f.read()
+
+        words += blob.split(' ')
+     #  print (ctr)
+        ctr -= 1
+
+    for i in range(len(words)):
+        if not words[i].isalpha():
+            words[i] = ""
+
+    dict = Counter(words)
+    del dict[""]
+    return (dict.most_common(3000))
 
 
-# Append directory email to file names
-emails = [direc + email for email in files]
+def make_dataset(dict):
+    direc = "email/"
+    files = os.listdir(direc)
+    # Append directory email to file names
+    emails = [direc + email for email in files]
 
-words = []
-ctr = len(emails)
-for email in emails:
-    f = open(email,  errors='ignore')
-    # Read email
-    blob = f.read()
+    labels = []
+    feature_set = []
+    ctr = len(emails)
+    for email in emails:
+        data = []
+        f = open(email,  errors='ignore')
+        words = f.read().split(' ')
+        for entry in dict:
+            data.append(words.count(entry[0]))
+        feature_set.append(data)
+        if "ham" in email:
+            labels.append(0)
+        if "spam" in email:
+            labels.append(1)
+            print(ctr)
+            ctr -= 1
+            return feature_set, labels
 
-    words += blob.split(' ')
+d = make_dict()
+features, labels = make_dataset(d)
 
-    ctr -= 1
-
-
-for i in range(len(words)):
-    if not words[i].isalpha():
-        words[i] = ""
-
-dict = Counter(words)
-
-print(dict.most_common(3000))
+print (len(features), len(labels))
